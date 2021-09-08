c21a3b46aac1e1536772e32eb1130dcd879abe0e
Baileys42 <87405840+Baileys42@users.noreply.github.com>
2021-09-08 17:11:44 +1000

    Added move file function

--- Trace_import.py
+++ Trace_import.py
@@ -8,9 +8,17 @@ import shutil
 
 
 
-data_path = 
+data_path = ""
+
+output = 
 
 
-def file_load(file):
-    trace_df = pd.DataFrame(np.loadtext(file))
-
+def get_trace_file(input_folder, output):
+    for folder in input_folder:
+        new_folder = folder.split("/")[-2]
+        if not os.path.exists(f"{output}{new_folder}/"):
+            os.makedirs(f"{output}{new_folder}/")
+        tracelist = [trace for trace in os.listdir(folder) if "dat" in trace]
+        for trace in tracelist:
+            shutil.copyfile(f"{folder}{trace}",f"{output}{new_folder}/{trace}")
+get_trace_file(data_path, output)
